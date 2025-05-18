a04ca8777d7c9d4402b5aed083b9d772b91a1c4d
Ferdin-Arsenic <arxenarendra@gmail.com>
2025-05-18 17:07:55 +0700

    feat : add combined heuristic

---
+++ src/algorithm/CombinedHeuristic.java
@@ -0,0 +1,40 @@
+package algorithm;
+
+import model.Board;
+import model.Piece;
+
+public class CombinedHeuristic implements Heuristic {
+
+    @Override
+    public int estimate(Board board) {
+        Piece primary = board.pieces.get('P');
+        if (primary == null || !primary.isHorizontal) return Integer.MAX_VALUE;
+
+        int row = primary.start.row;
+        int endCol = primary.start.col + primary.length - 1;
+
+        // Komponen 1: Manhattan Distance to Exit
+        int distanceToExit = Math.abs(endCol - (board.cols - 1));
+
+        // Komponen 2: Blocking Cars
+        int blockingPenalty = 0;
+        int weightedBlocking = 0;
+        for (int col = endCol + 1; col < board.cols; col++) {
+            char cell = board.grid[row][col];
+            if (cell != '.' && cell != 'K') {
+                blockingPenalty++;
+                weightedBlocking += (board.cols - col);  // Prioritaskan yang dekat
+            }
+        }
+
+        // Gabungan (bobot dapat disesuaikan)
+        int result = distanceToExit + (2 * blockingPenalty) + weightedBlocking;
+
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "CombinedHeuristic";
+    }
+}
--- src/ui/GUI.java
+++ src/ui/GUI.java
@@ -14,6 +14,7 @@ import algorithm.IDDFS;
 import algorithm.ManhattanToExitHeuristic;
 import algorithm.Pathfinder;
 import algorithm.UCS;
+import algorithm.CombinedHeuristic;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
@@ -272,6 +273,11 @@ public class GUI extends Application {
         manhattanRadio.setUserData("Manhattan To Exit");
         manhattanRadio.setFont(Font.font("Arial", 16));
 
+        RadioButton combinedRadio = new RadioButton("Combined Heuristic");
+        combinedRadio.setToggleGroup(heuristicToggleGroup);
+        combinedRadio.setUserData("Combined Heuristic");
+        combinedRadio.setFont(Font.font("Arial", 16));
+
         blockingCarsRadio.setSelected(true);
         this.heuristic = "Blocking Cars";
 
@@ -282,7 +288,7 @@ public class GUI extends Application {
             }
         });
 
-        VBox radioButtonBox = new VBox(10, blockingCarsRadio, manhattanRadio);
+        VBox radioButtonBox = new VBox(10, blockingCarsRadio, manhattanRadio, combinedRadio);
         radioButtonBox.setAlignment(Pos.CENTER_LEFT);
 
         HBox radioContainer = new HBox(radioButtonBox);
@@ -446,12 +452,16 @@ public class GUI extends Application {
                 solver = new GreedyBestFirst(new BlockingCarsHeuristic());
             } else if ("Manhattan To Exit".equals(this.heuristic)) {
                 solver = new GreedyBestFirst(new ManhattanToExitHeuristic());
+            } else if("Combined Heuristic".equals(this.heuristic)){
+                solver = new GreedyBestFirst(new CombinedHeuristic());
             }
         } else if ("A* Search".equals(this.algorithm)) {
             if ("Blocking Cars".equals(this.heuristic)) {
                 solver = new AStar(new BlockingCarsHeuristic());
             } else if ("Manhattan To Exit".equals(this.heuristic)) {
                 solver = new AStar(new ManhattanToExitHeuristic());
+            } else if("Combined Heuristic".equals(this.heuristic)){
+                solver = new AStar(new CombinedHeuristic());
             }
         } else if ("Iterative Deepening Search".equals(this.algorithm)){
             solver = new IDDFS();
